7ef7e962a66b41b705d99b9308d47d2c32cb7a89
Yair Riascos <yeiror@gmail.com>
2021-10-29 20:03:55 -0500

    Creación del servidor del Backend y post funcionando

--- Backend/app.js
+++ Backend/app.js
@@ -3,15 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var database = require("./config/database");
+var usuariosRouter = require('./routes/usuarios.router')
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,10 +14,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//Mongo Connection
+database.mongoConnnect();
 
-// catch 404 and forward to error handler
+// rutas
+app.use('/usuarios',usuariosRouter);
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
@@ -37,5 +34,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
