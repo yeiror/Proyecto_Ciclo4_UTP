a44e97c4ea81ba12b9a69a6a7400c80f5d3743cc
Alejandro Jimenez <alejandroedu71@gmail.com>
2021-11-07 10:50:48 -0500

    Desarrollo de barra de navegación

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import logo from './assets/logo.png';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import {MDBIcon, MDBBadge} from 'mdb-react-ui-kit';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" expand="lg"  sticky="top">
+  <Container fluid>
+  <Navbar.Brand href="#home" className='mb-0 h2' >
+        <img
+          alt=""
+          src={logo}
+          width="50"
+          height="50"
+          className="d-inline-block align-top"
+        />{' '}
+      SNEAKER STORE
+      </Navbar.Brand>
+
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="justify-content-center" >
+        <Nav.Link href="#hombre">Hombre</Nav.Link>
+        <Nav.Link href="#mujer">Mujer</Nav.Link>
+        <Nav.Link href="#buscar">|Buscar <MDBIcon fas icon="search" />|</Nav.Link>
+        <Nav.Link href="#carro_compras"><MDBIcon fas icon='shopping-cart'></MDBIcon>
+              <span>
+              <MDBBadge pill color='danger'>!</MDBBadge>
+              </span></Nav.Link>
+        <Nav.Link href="#sesion">Iniciar Sesión</Nav.Link>
+
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+
+
+    
     </div>
   );
 }
