68ed2960258ff06c6f82d8ec337c4c329add688a
Lauracaro96 <laura.caro.0814@gmail.com>
2021-11-27 15:41:40 -0500

    Se mejoran tarjetas del catalogo hombre y mujer

--- frontend/src/components/card_hombre.js
+++ frontend/src/components/card_hombre.js
@@ -1,160 +1,31 @@
 import React from "react";
 import { Container, Card, Row, Col, Button } from "react-bootstrap";
 import Piepagina from "./footer";
+import cards from "../json/card_hombre.json"
 
 function Catalogohombre(props) {
     return (
     <div>
     
     <Row xs={1} md={4} className="g-4">
-    {Array.from({ length: 1 }).map((_, idx) => (
+    {cards.map((card) =>(
     <Col>
         <Card>
-        <Card.Img variant="top" src="https://static.dafiti.com.co/p/adidas-performance-7474-7426621-1-catalog-new.jpg" />
+        <Card.Img variant="top" src={card.imagen} />
         <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title>{card.titulo}</Card.Title>
             <Card.Text>
-            <Button className="ButtonTalla" variant="info">34</Button>
-            <Button variant="info">36</Button>
-            <Button variant="info">38</Button>
-            $230.000
+            <Button className="ButtonTalla" variant="info">{card.talla1}</Button>
+            <Button variant="info">{card.talla2}</Button>
+            <Button variant="info">{card.talla3}</Button>
+            <Button variant="info">{card.talla4}</Button>
+            ${card.precio}
             </Card.Text>
             <Button variant="primary">Agrega al carrito</Button>
         </Card.Body>
         </Card>
     </Col>
-    ))}
 
-
-{Array.from({ length: 1 }).map((_, idx) => (
-    <Col>
-        <Card>
-        <Card.Img variant="top" src="https://static.dafiti.com.co/p/royal-county-of-berkshire-polo-club-8248-3405741-1-catalog-new.jpg" />
-        <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>
-            <Button className="ButtonTalla" variant="info">34</Button>
-            <Button variant="info">36</Button>
-            <Button variant="info">38</Button>
-            $230.000
-            </Card.Text>
-            <Button variant="primary">Agrega al carrito</Button>
-        </Card.Body>
-        </Card>
-    </Col>
-    ))}
-
-{Array.from({ length: 1 }).map((_, idx) => (
-    <Col>
-        <Card>
-        <Card.Img variant="top" src="https://static.dafiti.com.co/p/adidas-performance-5683-2596921-1-catalog-new.jpg" />
-        <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>
-            <Button className="ButtonTalla" variant="info">34</Button>
-            <Button variant="info">36</Button>
-            <Button variant="info">38</Button>
-            $230.000
-            </Card.Text>
-            <Button variant="primary">Agrega al carrito</Button>
-        </Card.Body>
-        </Card>
-    </Col>
-    ))}
-
-
-{Array.from({ length: 1 }).map((_, idx) => (
-    <Col>
-        <Card>
-        <Card.Img variant="top" src="https://static.dafiti.com.co/p/under-armour-3219-2617941-1-catalog-new.jpg" />
-        <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>
-            <Button className="ButtonTalla" variant="info">34</Button>
-            <Button variant="info">36</Button>
-            <Button variant="info">38</Button>
-            $230.000
-            </Card.Text>
-            <Button variant="primary">Agrega al carrito</Button>
-        </Card.Body>
-        </Card>
-    </Col>
-    ))}
-    </Row>
-
-    <Row xs={1} md={4} className="g-4">
-    {Array.from({ length: 1 }).map((_, idx) => (
-    <Col>
-        <Card>
-        <Card.Img variant="top" src="https://static.dafiti.com.co/p/tellenzi-2448-4361141-1-catalog-new.jpg" />
-        <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>
-            <Button className="ButtonTalla" variant="info">34</Button>
-            <Button variant="info">36</Button>
-            <Button variant="info">38</Button>
-            $230.000
-            </Card.Text>
-            <Button variant="primary">Agrega al carrito</Button>
-        </Card.Body>
-        </Card>
-    </Col>
-    ))}
-
-
-{Array.from({ length: 1 }).map((_, idx) => (
-    <Col>
-        <Card>
-        <Card.Img variant="top" src="https://static.dafiti.com.co/p/asics-3090-7447171-1-catalog-new.jpg" />
-        <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>
-            <Button className="ButtonTalla" variant="info">34</Button>
-            <Button variant="info">36</Button>
-            <Button variant="info">38</Button>
-            $230.000
-            </Card.Text>
-            <Button variant="primary">Agrega al carrito</Button>
-        </Card.Body>
-        </Card>
-    </Col>
-    ))}
-
-{Array.from({ length: 1 }).map((_, idx) => (
-    <Col>
-        <Card>
-        <Card.Img variant="top" src="https://static.dafiti.com.co/p/treme-2381-3890771-1-catalog-new.jpg" />
-        <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>
-            <Button className="ButtonTalla" variant="info">34</Button>
-            <Button variant="info">36</Button>
-            <Button variant="info">38</Button>
-            $230.000
-            </Card.Text>
-            <Button variant="primary">Agrega al carrito</Button>
-        </Card.Body>
-        </Card>
-    </Col>
-    ))}
-
-
-{Array.from({ length: 1 }).map((_, idx) => (
-    <Col>
-        <Card>
-        <Card.Img variant="top" src="https://static.dafiti.com.co/p/nike-3737-7810841-1-catalog-new.jpg" />
-        <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>
-            <Button className="ButtonTalla" variant="info">34</Button>
-            <Button variant="info">36</Button>
-            <Button variant="info">38</Button>
-            $230.000
-            </Card.Text>
-            <Button variant="primary">Agrega al carrito</Button>
-        </Card.Body>
-        </Card>
-    </Col>
     ))}
     </Row>
     
--- frontend/src/components/card_mujer.js
+++ frontend/src/components/card_mujer.js
@@ -1,160 +1,31 @@
 import React from "react";
 import { Container, Card, Row, Col, Button } from "react-bootstrap";
 import Piepagina from "./footer";
+import cards from "../json/card_mujer.json"
 
 function Catalogomujer(props) {
     return (
     <div>
     
     <Row xs={1} md={4} className="g-4">
-    {Array.from({ length: 1 }).map((_, idx) => (
+    {cards.map((card) =>(
     <Col>
         <Card>
-        <Card.Img variant="top" src="https://static.dafiti.com.co/p/skechers-4252-7036931-1-catalog-new.jpg" />
+        <Card.Img variant="top" src={card.imagen} />
         <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title>{card.titulo}</Card.Title>
             <Card.Text>
-            <Button className="ButtonTalla" variant="info">34</Button>
-            <Button variant="info">36</Button>
-            <Button variant="info">38</Button>
-            $230.000
+            <Button className="ButtonTalla" variant="info">{card.talla1}</Button>
+            <Button variant="info">{card.talla2}</Button>
+            <Button variant="info">{card.talla3}</Button>
+            <Button variant="info">{card.talla4}</Button>
+            ${card.precio}
             </Card.Text>
             <Button variant="primary">Agrega al carrito</Button>
         </Card.Body>
         </Card>
     </Col>
-    ))}
 
-
-{Array.from({ length: 1 }).map((_, idx) => (
-    <Col>
-        <Card>
-        <Card.Img variant="top" src="https://static.dafiti.com.co/p/skechers-1199-2296931-1-catalog-new.jpg" />
-        <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>
-            <Button className="ButtonTalla" variant="info">34</Button>
-            <Button variant="info">36</Button>
-            <Button variant="info">38</Button>
-            $230.000
-            </Card.Text>
-            <Button variant="primary">Agrega al carrito</Button>
-        </Card.Body>
-        </Card>
-    </Col>
-    ))}
-
-{Array.from({ length: 1 }).map((_, idx) => (
-    <Col>
-        <Card>
-        <Card.Img variant="top" src="https://static.dafiti.com.co/p/skechers-0433-6705521-1-catalog-new.jpg" />
-        <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>
-            <Button className="ButtonTalla" variant="info">34</Button>
-            <Button variant="info">36</Button>
-            <Button variant="info">38</Button>
-            $230.000
-            </Card.Text>
-            <Button variant="primary">Agrega al carrito</Button>
-        </Card.Body>
-        </Card>
-    </Col>
-    ))}
-
-
-{Array.from({ length: 1 }).map((_, idx) => (
-    <Col>
-        <Card>
-        <Card.Img variant="top" src="https://static.dafiti.com.co/p/royal-county-of-berkshire-polo-club-4336-3789351-1-catalog-new.jpg" />
-        <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>
-            <Button className="ButtonTalla" variant="info">34</Button>
-            <Button variant="info">36</Button>
-            <Button variant="info">38</Button>
-            $230.000
-            </Card.Text>
-            <Button variant="primary">Agrega al carrito</Button>
-        </Card.Body>
-        </Card>
-    </Col>
-    ))}
-    </Row>
-
-    <Row xs={1} md={4} className="g-4">
-    {Array.from({ length: 1 }).map((_, idx) => (
-    <Col>
-        <Card>
-        <Card.Img variant="top" src="https://static.dafiti.com.co/p/royal-county-of-berkshire-polo-club-2287-3489351-1-catalog-new.jpg" />
-        <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>
-            <Button className="ButtonTalla" variant="info">34</Button>
-            <Button variant="info">36</Button>
-            <Button variant="info">38</Button>
-            $230.000
-            </Card.Text>
-            <Button variant="primary">Agrega al carrito</Button>
-        </Card.Body>
-        </Card>
-    </Col>
-    ))}
-
-
-{Array.from({ length: 1 }).map((_, idx) => (
-    <Col>
-        <Card>
-        <Card.Img variant="top" src="https://static.dafiti.com.co/p/royal-county-of-berkshire-polo-club-8099-6458321-1-catalog-new.jpg" />
-        <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>
-            <Button className="ButtonTalla" variant="info">34</Button>
-            <Button variant="info">36</Button>
-            <Button variant="info">38</Button>
-            $230.000
-            </Card.Text>
-            <Button variant="primary">Agrega al carrito</Button>
-        </Card.Body>
-        </Card>
-    </Col>
-    ))}
-
-{Array.from({ length: 1 }).map((_, idx) => (
-    <Col>
-        <Card>
-        <Card.Img variant="top" src="https://static.dafiti.com.co/p/adidas-performance-1963-4327771-1-catalog-new.jpg" />
-        <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>
-            <Button className="ButtonTalla" variant="info">34</Button>
-            <Button variant="info">36</Button>
-            <Button variant="info">38</Button>
-            $230.000
-            </Card.Text>
-            <Button variant="primary">Agrega al carrito</Button>
-        </Card.Body>
-        </Card>
-    </Col>
-    ))}
-
-
-{Array.from({ length: 1 }).map((_, idx) => (
-    <Col>
-        <Card>
-        <Card.Img variant="top" src="https://static.dafiti.com.co/p/royal-county-of-berkshire-polo-club-6831-5994741-1-catalog-new.jpg" />
-        <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>
-            <Button className="ButtonTalla" variant="info">34</Button>
-            <Button variant="info">36</Button>
-            <Button variant="info">38</Button>
-            $230.000
-            </Card.Text>
-            <Button variant="primary">Agrega al carrito</Button>
-        </Card.Body>
-        </Card>
-    </Col>
     ))}
     </Row>
     
